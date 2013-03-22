df9315eeb546527c6e8a96b9467d6476faad1d7a
Björn Ekengren <bjorn.ekengren@gmail.com>
2013-03-22 13:21:04 +0100

    Refactoring

--- src/main/webapp/script/communication.js
+++ src/main/webapp/script/communication.js
@@ -1,13 +1,16 @@
-var communication = (function(){
+var comlink = (function(){
+    var socket,
+        url = window.location.origin.match(/[^http://].+[^\d]/);
 
-    function init( callback ){
-        var socket = new WebSocket("ws://localhost:9000/");
+    function init( callback , messageHandler){
+        socket = new WebSocket("ws://" + url + "8090/");
         socket.onopen = function () {
             console.log("Socket has been opened, spawning evil...");
-            socket.onmessage = function (msg) {
-                console.log('Got message:')
-                console.log(msg);
-            }
+            socket.onmessage = messageHandler;
+//                function (msg) {
+//                    console.log('Got message:')
+//                    console.log(msg);
+//                }
             callback();
         }
     }
@@ -28,9 +31,9 @@ var communication = (function(){
     }
 })();
 
-communication.init(function(){
-    $('#send-button').on('click', function(e){
-        communication.send($('#text').val());
-    });
+comlink.init(function(){
+//    $('#send-button').on('click', function(e){
+//        comlink.send($('#text').val());
+//    });
 });
 
