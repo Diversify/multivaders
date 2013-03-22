c7dc733185890efaed932e557e236289c32ca28c
Uzi Landsmann <uzi.landsmann@gmail.com>
2013-03-22 13:28:05 +0100

    Fixed bug in BasedOnPrevoiusStrategy

--- src/main/java/se/diversify/multivaders/strategy/BasedOnPreviousStrategy.java
+++ src/main/java/se/diversify/multivaders/strategy/BasedOnPreviousStrategy.java
@@ -21,6 +21,7 @@ public class BasedOnPreviousStrategy extends AbstractStrategy {
                 toReturn = newEvent;
             }
         }
+        oldEvent = newEvent;
         decisionMaker.sendResponse(toReturn);
     }
 }
